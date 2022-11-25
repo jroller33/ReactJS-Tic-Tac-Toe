c8f37e7ff853c701755508164c9631f7e2c96c0e
John Roller <jtroller33@gmail.com>
2022-11-24 21:03:34 -0600

    Board now ctrls Square components

--- src/index.js
+++ src/index.js
@@ -10,20 +10,13 @@ import './index.css';
 
 class Square extends React.Component {        //    SQUARE
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button 
       className="square" 
-      onClick={() => this.setState({ value: 'X' })}
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -43,8 +36,19 @@ class Board extends React.Component {             //    BOARD
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+    />
+    );
   }
 
   render() {
